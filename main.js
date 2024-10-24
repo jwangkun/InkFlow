f0e0e21f1cae829eb862888e8d8bce81e46d7fea
jwangkun <156646047@qq.com>
2024-10-24 14:52:51 +0800

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  // 开发环境下加载 Vite dev server
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:3000')
+  } else {
+    win.loadFile('dist/index.html')
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
